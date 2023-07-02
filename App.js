6254ef4402d2a3183eea8b29b534c1b3ed1a8966
sanninglue <leon19831233@gmail.com>
2023-07-02 06:04:41 +0000

    commit

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from "./screens/WelcomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        screenOptions={{ headerShown: false, gestureEnabled: false,
+          cardStyleInterpolator: ({ current, layouts }) => ({
+            cardStyle: {
+              transform: [
+                {
+                  translateX: current.progress.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [layouts.screen.width, 0],
+                  }),
+                },
+              ],
+            },
+          }),
+        }}
+        initialRouteName="Welcome"
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
---
+++ screens/LoginScreen.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Login</Text>
+        <Image
+          style={styles.logo}
+          source={require("../assets/logoblack.png")} 
+        />
+      </View>
+      <TextInput 
+        style={styles.input}
+        placeholder="Email" 
+      />
+      <TextInput 
+        style={styles.input}
+        placeholder="Password" 
+        secureTextEntry 
+      />
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.createAccountButton} onPress={() => {}}>
+          <Text style={styles.buttonText}>Create account</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.signInButton} onPress={() => {}}>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  title: {
+    fontSize: 60,
+    marginRight: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+    minWidth: 200,
+    width: '100%',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  buttons: {
+    width: '100%',
+  },
+  createAccountButton: {
+    backgroundColor: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 25,  
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',  
+    marginBottom: 10, 
+  },
+  signInButton: {
+    backgroundColor: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 25,  
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%', 
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 18,
+  }
+});
+
+export default LoginScreen;
