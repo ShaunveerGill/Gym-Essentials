f45ed9c1b3e1caed346178258206ac4631d044f7
Courtney Wei <clwei@ucsc.edu>
2023-07-19 19:03:17 -0700

    fixed-ish refractoring login

--- src/data/userServices.js
+++ src/data/userServices.js
@@ -1,5 +1,8 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
+import { auth } from "../../firebase";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { UserContext } from "../context/UserContext";
 
 export const updateData = (variableName, value) => {
   const user = firebase.auth().currentUser;
@@ -19,77 +22,47 @@ export const updateData = (variableName, value) => {
     });
 };
 
+export const handleLogin = (userEmail, password, setUserEmail, setUserName, setGender, setAge, setHeight, setWeight, setGoal, setActivityLevel) => {
+  return new Promise((resolve, reject) => {
+    auth.signInWithEmailAndPassword(userEmail, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        const userData = auth.currentUser;
 
+        if (userData !== null) {
+          setUserEmail(userData.email);
+          const db = getDatabase();
+          const userRef = ref(db, "users/" + userData.uid);
 
-// export const handleLogin = () => {
-//     auth
-//       .signInWithEmailAndPassword(userEmail, password)
-//       .then(userCredentials => {
-//         const user = userCredentials.user;
-//         const userData = auth.currentUser;
-//         if (userData !== null) {
-//           setUserEmail(userData.email);
-//           const db = getDatabase();
-//           const userRef = ref(db, "users/" + userData.uid);
-//           onValue(userRef, (snapshot) => {
-//             const data = snapshot.val();
-//             if (data !== null) {
-//               setUserName(data.name);
-//               setGender(data.gender);
-//               setAge(data.age);
-//               setHeight(data.height);
-//               setWeight(data.weight);
-//               setGoal(data.goal);
-//               setActivityLevel(data.activityLevel);
-//             }
-//           });
-//           setPassword('');
-//           navigation.navigate('FeaturesOverview');
-//         }
-//       })
-//       .catch(error => alert(error.message))
-// }
-// const handleFinishButtonPress = () => {
-//     // Check for the current user
-//     // if (!Incomplete) {
-//     setSub(true);
+          onValue(
+            userRef,
+            (snapshot) => {
+              const data = snapshot.val();
 
-//     const user = auth.currentUser;
-//     verification = true;
+              if (data !== null) {
+                setUserName(data.name);
+                setGender(data.gender);
+                setAge(data.age);
+                setHeight(data.height);
+                setWeight(data.weight);
+                setGoal(data.goal);
+                setActivityLevel(data.activityLevel);
+              }
 
-//     if (
-//       gender &&
-//       userName &&
-//       age &&
-//       height &&
-//       weight &&
-//       goal &&
-//       activityLevel
-//     ) {
-//       if (user) {
-//         const userData = {
-//           email: userEmail,
-//           name: userName,
-//           gender: gender,
-//           age: age,
-//           height: height,
-//           weight: weight,
-//           goal: goal,
-//           activityLevel: activityLevel,
-//         };
-
-//         const db = getDatabase();
-//         const userRef = ref(db, "users/" + user.uid);
-
-//         set(userRef, userData)
-//           .then(() => {
-//             navigation.navigate("FeaturesOverview");
-//           })
-//           .catch((error) => {
-//             console.error("Error saving user data: ", error);
-//           });
-//       } else {
-//         console.error("No user is signed in");
-//       }
-//     }
-//   };
+              resolve(); // Resolve the Promise after updating user data
+            },
+            (error) => {
+              // Error occurred while fetching user data
+              reject(error); // Reject the Promise with the error
+            }
+          );
+        }
+      })
+      .catch((error) => {
+        // Handle authentication errors and show pop-up warning
+        const errorMessage = error.message || "An error occurred during login.";
+        alert(errorMessage);
+        reject(error); // Reject the Promise with the error
+      });
+  });
+};
--- src/screens/FirstScreens/LoginScreen.js
+++ src/screens/FirstScreens/LoginScreen.js
@@ -4,13 +4,11 @@ import { useNavigation } from '@react-navigation/native';
 import { getDatabase, ref, onValue } from "firebase/database";
 import { auth } from '../../../firebase'
 import { UserContext } from "../../context/UserContext";
-
+import { handleLogin } from "../../data/userServices";
 
 function LoginScreen() {
-  //replace with ionicons
-  const mailSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="grey" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>`;
-  const lockSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="grey" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>`;
 
+  //remove loginERROr
   const navigation = useNavigation();
   const {
     userEmail,
@@ -24,35 +22,20 @@ function LoginScreen() {
     setActivityLevel,
   } = useContext(UserContext);
   const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(null); 
 
-  const handleLogin = () => {
-    auth
-      .signInWithEmailAndPassword(userEmail, password)
-      .then(userCredentials => {
-        const user = userCredentials.user;
-        const userData = auth.currentUser;
-        if (userData !== null) {
-          setUserEmail(userData.email);
-          const db = getDatabase();
-          const userRef = ref(db, "users/" + userData.uid);
-          onValue(userRef, (snapshot) => {
-            const data = snapshot.val();
-            if (data !== null) {
-              setUserName(data.name);
-              setGender(data.gender);
-              setAge(data.age);
-              setHeight(data.height);
-              setWeight(data.weight);
-              setGoal(data.goal);
-              setActivityLevel(data.activityLevel);
-            }
-          });
-          setPassword('');
-          navigation.navigate('FeaturesOverview');
-        }
+  const handleSubmit = () => {
+    handleLogin(userEmail, password, setUserEmail, setUserName, setGender, setAge, setHeight, setWeight, setGoal, setActivityLevel)
+      .then(() => {
+        setPassword('');
+        setLoginError(null); 
+        navigation.navigate('FeaturesOverview');
       })
-      .catch(error => alert(error.message))
+      .catch((error) => {
+        setLoginError(error.message);
+      });
   }
+
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -75,7 +58,7 @@ function LoginScreen() {
           secureTextEntry
         />
         <View style={styles.buttons}>
-          <TouchableOpacity style={styles.createAccountButton} onPress={handleLogin}>
+          <TouchableOpacity style={styles.createAccountButton} onPress={handleSubmit}>
             <Text style={styles.buttonText}>Login</Text>
           </TouchableOpacity>
         </View>
